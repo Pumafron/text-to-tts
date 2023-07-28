c8cf09d33ef7b6542377aa1a9428e3c76bf860cf
pumafron <pumafron@gmail.com>
2023-07-27 21:32:28 -0600

    microfix 1.2

--- getAvalaibleVoices.py
+++ getAvalaibleVoices.py
@@ -1,23 +1,9 @@
+import json
 from flask import Blueprint,jsonify
-import subprocess
 
 urls6_blueprint = Blueprint('getVoicesAvalaible', __name__,)
 @urls6_blueprint.route('/')
-def getvoices():
-    process = subprocess.Popen(['balcon','-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = process.communicate()[0]
-    processed = out.decode("u8").replace("\r","").replace("  ","").split("\n")
-    dicc = {
-        "None":[],
-        "SAPI 4:":[],
-        "SAPI 5:":[]
-    }
-    actualValue = "None"
-    for i in processed:
-        if(i == "SAPI 4:" or i == "SAPI 5:"):
-            actualValue = i
-            continue
-        dicc[actualValue].append(i)
-
-    del dicc['None']
-    return jsonify(dicc)
+def getvoicesAvalaible():
+     with open('voices.json') as json_file:
+        voices : dict = json.load(json_file)
+        return jsonify(voices)
