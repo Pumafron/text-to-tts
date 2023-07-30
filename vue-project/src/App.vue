74a5d9cdb7b2dcc3866fada2e0fb4f2b847c7bb5
pumafron <pumafron@gmail.com>
2023-07-29 19:59:46 -0600

    version 1

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import {ref} from "vue";
+  import voice from "./components/voice.vue"
+</script>
+<template>
+  <header class="header">
+    <h1>app text to tts</h1>
+  </header>
+  <main class="content">
+    <div>
+      <h2>config and text area</h2>
+    </div>
+    <div>
+      <h2>avalaibleVoices</h2>
+      <voice></voice>
+      <voice></voice>
+      <voice></voice>
+      <voice></voice>
+      <voice></voice>
+      <voice></voice>
+      <voice></voice>
+    </div>
+  </main>
+</template>
+<style>
+  .content{
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+  }
+  .header{
+    text-align: center;
+  }
+</style>
---
+++ vue-project/src/components/voice.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+import propriety from "./propriety.vue"
+const nameT = "{{name}}"
+const showArticle = ref(false);
+const swapVisibility = () => {
+  if (showArticle.value ){
+    showArticle.value = false
+  } else {
+    showArticle.value = true
+  }
+}
+</script>
+
+<template>
+  <div class="nametag">
+    <div class="nametag__header">
+      <p>{{nameT}}</p>
+      <button @click="swapVisibility">hide</button>
+    </div>
+  <div>
+    <article v-show="showArticle" class="nametag__article">
+     <section>
+     <propriety></propriety>
+     <propriety></propriety>
+     <propriety></propriety>
+     <propriety></propriety>
+     <propriety></propriety>
+     </section>
+     <section><img src="@/assets/profilePIC.png" alt="profile pic"></section>
+    </article>
+  </div>
+
+  </div>
+</template>
+<style>
+  .nametag{
+  background-color: bisque;
+    display: flex;
+    flex-direction: column;
+  }
+  .nametag__header{
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+  }
+  .nametag__header button{
+    color: rgb(124, 80, 22);
+    max-width: 50px;
+  }
+  .nametag__article{
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    background-color: aquamarine;
+  }
+</style>
